d5f65531ae19f20bd8016b9765a76ca78ef32ce1
Rohit301005 <rohitkumarrrawat04@gmail.com>
2025-11-06 22:41:56 +0530

    bubble

--- CH-1 Sorting/bubble.py
+++ CH-1 Sorting/bubble.py
@@ -2,11 +2,17 @@ n = int(input())
 myList = [int(ele) for ele in input().split()]
 
 for i in range(n-1):
+    isSwaped = False
     for j in range(n-i-1):
         if myList[j] > myList[j+1]:
+            isSwaped = True
             temp = myList[j]
             myList[j] = myList[j+1]
             myList[j+1] = temp
+    
+    if isSwaped == False:
+        break
+    
 
 
 print(myList)
